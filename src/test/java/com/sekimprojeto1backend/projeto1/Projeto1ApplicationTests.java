b6972208d92f0068ac34819f0af8a26956295e49
Saloma kim <sekimzero.dev@gmail.com>
2023-09-07 15:43:28 -0300

    Project created

---
+++ src/test/java/com/sekimprojeto1backend/projeto1/Projeto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekimprojeto1backend.projeto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
